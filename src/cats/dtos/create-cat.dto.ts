d0e73fe03cf9780cb65912bbc64c9679382c7463
Natthakornn <yhai0079@gmail.com>
2023-08-11 23:35:02 +0700

    add validation for data objects transfer.

--- src/cats/dtos/create-cat.dto.ts
+++ src/cats/dtos/create-cat.dto.ts
@@ -1,6 +1,17 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateCatDto {
     id : string;
+
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsNumber()
+    @IsNotEmpty()
     age: number;
+
+    @IsString()
+    @IsNotEmpty()
     breed: string;
   }
